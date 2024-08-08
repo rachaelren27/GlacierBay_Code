b75ff622f54d23001a5ffc3286c8acb19c753920
Rachael Ren <rachaelren@live.com>
2024-08-08 13:05:03 -0700

    set up X matrices

--- SPP_script.R
+++ SPP_script.R
@@ -7,6 +7,7 @@ library(terra)
 library(raster)
 library(hilbertSimilarity)
 library(geosphere)
+library(tictoc)
 
 # --- Read in NPS data ---------------------------------------------------------
 path <- here("NPS_data", "HARBORSEAL_2007", "seal_locations_final",
@@ -29,9 +30,17 @@ seal.locs <- st_transform(seal.locs.20070813$geometry,
 footprint <- st_transform(footprint.20070813$geometry, 
                           CRS("+proj=longlat +datum=WGS84"))
 
+# prepare windows
 survey.poly.mat <- survey.poly[[1]][[1]]
 survey.win <- owin(poly = data.frame(x=rev(survey.poly.mat[,1]),
                                      y=rev(survey.poly.mat[,2])))
+
+footprints <- lapply(1:length(footprint), function(i) {
+  footprint.mat <- footprint[[i]][[1]]
+  owin(poly = data.frame(x=rev(footprint.mat[,1]),
+                         y=rev(footprint.mat[,2])))
+})
+footprint.win <- do.call(union.owin, footprints)
 
 # plot
 ggplot() + 
@@ -39,7 +48,7 @@ ggplot() +
   geom_sf(data = footprint) + 
   geom_sf(data = seal.locs, size = 0.5)
 
-# --- Read in Covariates -------------------------------------------------------
+# --- Read in covariates -------------------------------------------------------
 # read in bathymetry
 bath.rast <- raster(here("covariates", "bathymetry.tiff"))
 
@@ -77,3 +86,36 @@ bath.survey.idx <- which(!is.na(values(bath.rast.survey)))
 full.coord <- xyFromCell(bath.rast.survey, bath.survey.idx)
 
 full.glac.dist <- dist2Line(full.coord, glacier.poly) # takes a while
+
+cor(na.omit(values(bath.rast.survey)), full.glac.dist[,1]) # -0.183
+
+# --- Calculate areas ----------------------------------------------------------
+tot.area <- area.owin(survey.win)
+tot.win.area <- area.owin(footprint.win)
+tot.nonwin.area <- tot.area - tot.win.area
+n.win <- length(footprint)
+win.area <- tot.win.area/n.win # approx. bc windows not equally sized
+
+# --- Set X matrices -----------------------------------------------------------
+bath <- na.omit(values(bath.rast.survey))
+glac.dist <- full.glac.dist[,1]
+
+seal.full.idx <- cellFromXY(bath.rast.survey, seal.mat)
+row.counts <- table(factor(seal.full.idx, levels = 1:length(bath.rast.survey)))
+bath.full <- cbind(values(bath.rast.survey), row.counts)
+bath <- na.omit(bath.full)
+X.full <- cbind(bath, glac.dist)
+seal.idx <- c()
+for(i in 1:nrow(X.full)){
+  if(X.full[i,2] != 0){
+    seal.idx <- c(seal.idx, rep(i, times = X.full[i,2]))
+  }
+}
+X.full <- scale(X.full[,-2])
+
+win.idx <- which(inside.owin(full.coord[,1], full.coord[,2], footprint.win))
+X.win.full <- X.full[win.idx,]
+
+X.obs <- X.full[seal.idx,]
+
+
